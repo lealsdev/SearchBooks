4fba64a6079dfa45990535a347a5b70f1bdf4d17
Leandro Alves Santos <lealsto82@gmail.com>
2020-05-25 03:44:48 -0300

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ResultsComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
