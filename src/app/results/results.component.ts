79648d19d45366111242e1605d04bfb1e9f9b545
Leandro Alves Santos <lealsto82@gmail.com>
2020-05-27 00:30:40 -0300

    Results component implementation

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SearchBooksService } from '../services/search-books.service';
+import { Book } from '../models/book';
 
 @Component({
   selector: 'app-results',
@@ -9,13 +11,26 @@ import { ActivatedRoute } from '@angular/router';
 export class ResultsComponent implements OnInit {
 
   searchString: String;
+  books: Book[] = [];
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private searchBooksService: SearchBooksService) { }
 
   ngOnInit(): void {
-    this.searchString = this.activatedRoute.snapshot.queryParams['searchString']
+    this.searchString = this.activatedRoute.snapshot.queryParams['searchString'];
 
-    console.log(this.searchString);
+    this.searchFreeBooks();
+  }
+
+  processNewSearch(): void {
+    this.books = [];
+    this.searchFreeBooks();
+  }
+
+  searchFreeBooks(): void {
+    this.searchBooksService.getFreeBooks(this.searchString).subscribe((data) => {
+      this.books.push(...data);
+    });    
   }
 
 }
